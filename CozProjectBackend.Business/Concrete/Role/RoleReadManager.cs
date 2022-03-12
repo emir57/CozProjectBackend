f4458566e3ae6bf283e83d47c73d6eabee8e3f69
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 16:53:11 +0300

    RoleReadManager

---
+++ CozProjectBackend.Business/Concrete/Role/RoleReadManager.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete
+{
+    public class RoleReadManager : IRoleReadService
+    {
+        private readonly IRoleReadDal _roleReadDal;
+
+        public RoleReadManager(IRoleReadDal roleReadDal)
+        {
+            _roleReadDal = roleReadDal;
+        }
+
+        public IDataResult<IQueryable<Role>> GetAll()
+        {
+            return new SuccessDataResult<IQueryable<Role>>(_roleReadDal.GetAll());
+        }
+
+        public async Task<IDataResult<Role>> GetByIdAsync(int id)
+        {
+            return new SuccessDataResult<Role>(await _roleReadDal.GetByIdAsync(id));
+        }
+    }
+}
