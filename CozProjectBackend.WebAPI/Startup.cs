7e608bbe37211746fab96529a6433013eeb06345
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 15:14:26 +0300

    Add Context and connection string

--- CozProjectBackend.WebAPI/Startup.cs
+++ CozProjectBackend.WebAPI/Startup.cs
@@ -1,7 +1,9 @@
+using CozProjectBackend.DataAccess.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace CozProjectBackend.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CozProjectDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MsSqlConnection")));
             services.AddControllers();
         }
 
