361dbf5ba80c3fa52c80e63760a170477f044e2e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 17:11:02 +0300

    Add AutofacBusinessModule

---
+++ CozProjectBackend.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.Business.Concrete;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EfRoleWriteDal>().As<IRoleWriteDal>();
+            builder.RegisterType<EfRoleReadDal>().As<IRoleReadDal>();
+
+            builder.RegisterType<IRoleReadService>().As<RoleReadManager>();
+            builder.RegisterType<IRoleWriteService>().As<RoleWriteManager>();
+            base.Load(builder);
+        }
+    }
+}
