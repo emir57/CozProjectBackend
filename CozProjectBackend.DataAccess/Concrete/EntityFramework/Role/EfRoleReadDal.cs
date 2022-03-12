910dfeb1ba77c8945984d8dde62753e75761c8d0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:56:48 +0300

    EfRoleReadDal and EfRoleWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Role/EfRoleReadDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using CozProjectBackend.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfRoleReadDal : EfReadRepository<Role>, IRoleReadDal
+    {
+        public EfRoleReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Role/EfRoleWriteDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using CozProjectBackend.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfRoleWriteDal : EfWriteRepository<Role>, IRoleWriteDal
+    {
+        public EfRoleWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
