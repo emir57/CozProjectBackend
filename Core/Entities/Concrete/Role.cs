b466fd94ab345cc91bd538537896ef60cb36356a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:25:06 +0300

    Add Role and User

---
+++ Core/Entities/Concrete/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    public class Role : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
