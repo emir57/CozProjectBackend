ec859eb022b76b6c682378c4785d454b16f20b35
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:54:18 +0300

    IRoleWriteDal And IRoleReadDal

---
+++ CozProjectBackend.DataAccess/Abstract/Role/IRoleReadDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Abstract
+{
+    public interface IRoleReadDal:IReadRepository<Role>
+    {
+    }
+}
