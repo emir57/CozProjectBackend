1e10a20fdd2b4ae3ffe60b6033353087f795faad
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:36:07 +0300

    Table

--- Core/DataAccess/EntityFramework/EfWriteRepository.cs
+++ Core/DataAccess/EntityFramework/EfWriteRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,6 +10,13 @@ namespace Core.DataAccess.EntityFramework
     public class EfWriteRepository<T> : IWriteRepository<T>
         where T : class, IEntity, new()
     {
+        private readonly DbContext _context;
+        public EfWriteRepository(DbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<T> Table => _context.Set<T>();
+
         public Task<int> Add(T entity)
         {
             throw new NotImplementedException();
