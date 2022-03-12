5e50d0207348de38ba3acd1cea6d9afc906402f9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 15:37:08 +0300

    IRoleWriteService

--- CozProjectBackend.Business/Abstract/Role/IRoleWriteService.cs
+++ CozProjectBackend.Business/Abstract/Role/IRoleWriteService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CozProjectBackend.Business.Abstract
 {
     public interface IRoleWriteService
     {
+        Task<IResult> AddAsync(Role entity);
+        IResult Update(Role entity);
+        IResult Delete(Role entity);
+        Task<int> SaveAsync();
     }
 }
