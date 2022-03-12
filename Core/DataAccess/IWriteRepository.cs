b73ab852d90c7a0c667f946591ded41ae7d034b8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:31:12 +0300

    IWriteRepository

---
+++ Core/DataAccess/IWriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IWriteRepository<T>
+        where T:class,IEntity,new()
+    {
+        Task<int> Add(T entity);
+        Task<int> Update(T entity);
+        Task<int> Delete(T entity);
+        Task SaveAsync();
+    }
+}
