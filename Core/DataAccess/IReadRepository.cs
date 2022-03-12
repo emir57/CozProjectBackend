56a2069f256a4b8313cee9691942d161bee74249
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:29:33 +0300

    IReadRepository

---
+++ Core/DataAccess/IReadRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IReadRepository<T>
+        where T:class,IEntity,new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        Task<IQueryable<T>> GetAllAsync();
+        Task<IQueryable<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+    }
+}
