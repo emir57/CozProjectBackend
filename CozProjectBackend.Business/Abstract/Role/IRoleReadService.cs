53741e968075cf2bc59cd46fae3ae6aee7077389
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 15:38:47 +0300

    IRoleReadService

--- CozProjectBackend.Business/Abstract/Role/IRoleReadService.cs
+++ CozProjectBackend.Business/Abstract/Role/IRoleReadService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CozProjectBackend.Business.Abstract
 {
     public interface IRoleReadService
     {
+        Task<IDataResult<Role>> GetByIdAsync(int id);
+        IDataResult<IQueryable<Role>> GetAll();
     }
 }
