d38e51e4b28226ac3d0e930f6cb225a12d5794e2
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 14:34:48 +0300

    IRepository

---
+++ Core/DataAccess/EntityFramework/EfWriteRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfWriteRepository<T> : IWriteRepository<T>
+        where T : class, IEntity, new()
+    {
+        public Task<int> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/DataAccess/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IRepository<T>
+        where T:class,IEntity,new()
+    {
+        DbSet<T> Table { get; }
+    }
+}
