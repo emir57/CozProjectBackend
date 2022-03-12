dd78f0679cc1dbf40a0201cd8a8a5d05180c0eb3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-12 15:17:39 +0300

    Role Controller

---
+++ CozProjectBackend.WebAPI/Controllers/RolesController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities.Concrete;
+using CozProjectBackend.DataAccess.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleWriteDal _roleWriteDal;
+        private readonly IRoleReadDal _roleReadDal;
+        public RolesController(IRoleReadDal roleReadDal, IRoleWriteDal roleWriteDal)
+        {
+            _roleReadDal = roleReadDal;
+            _roleWriteDal = roleWriteDal;
+        }
+        [HttpPost("add")]
+        public async Task<IActionResult> Add(Role role)
+        {
+            bool result = await _roleWriteDal.AddAsync(role);
+            return Ok(result);
+        }
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            IQueryable<Role> result = _roleReadDal.GetAll();
+            return Ok(result);
+        }
+    }
+}
