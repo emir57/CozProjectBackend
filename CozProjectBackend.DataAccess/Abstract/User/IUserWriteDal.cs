2515662d14d248002e2dee675c4c516dadd924eb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:39:52 +0300

    IUserWriteDal

---
+++ CozProjectBackend.DataAccess/Abstract/User/IUserWriteDal.cs
@@ -0,0 +1,10 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+
+namespace CozProjectBackend.DataAccess.Abstract
+{
+    public interface IUserWriteDal : IWriteRepository<User>
+    {
+
+    }
+}
