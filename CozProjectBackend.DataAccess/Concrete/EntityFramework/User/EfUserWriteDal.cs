f89b9e908ee14cd9f3721940aa99e030e5090b8d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:41:29 +0300

    EfUserReadDal and EfUserWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/User/EfUserWriteDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using CozProjectBackend.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfUserWriteDal : EfWriteRepository<User>, IUserWriteDal
+    {
+        public EfUserWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
