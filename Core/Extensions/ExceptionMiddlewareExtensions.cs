a78083725f2654504837f8ab746edd9a62e11ee4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:37:29 +0300

    ExceptionMiddlewareExtensions

---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Middleware;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+            return app;
+        }
+    }
+}
