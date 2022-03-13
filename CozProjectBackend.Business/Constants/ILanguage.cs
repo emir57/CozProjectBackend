5ef6499a911a2a42338590498643efb57a3c0819
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 15:01:09 +0300

    TurkishLanguage

---
+++ CozProjectBackend.Business/Constants/ILanguage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Constants
+{
+    public interface ILanguage
+    {
+        string SuccessAdd { get; }
+        string SuccessDelete { get;}
+        string SuccessUpdate { get;}
+
+        string FailureAdd { get;}
+        string FailureUpdate { get;}
+        string FailureDelete { get;}
+
+        string SuccessGet { get;}
+        string FailureGet { get;}
+
+        string UnAuthorize { get;}
+    }
+}
---
+++ CozProjectBackend.Business/Constants/TurkishLanguage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Constants
+{
+    public class TurkishLanguage : ILanguage
+    {
+        public string SuccessAdd => "Ekleme Başarılı";
+
+        public string SuccessDelete => "Silme Başarılı";
+
+        public string SuccessUpdate => "Güncelleme Başarılı";
+
+        public string FailureAdd => "Ekleme Başarısız";
+
+        public string FailureUpdate => "Güncelleme Başarısız";
+
+        public string FailureDelete => "Silme Başarısız";
+
+        public string SuccessGet => "Listeleme Başarılı";
+
+        public string FailureGet => "Listeleme Başarısız";
+
+        public string UnAuthorize => "Yetkisiz İşlem";
+    }
+}
