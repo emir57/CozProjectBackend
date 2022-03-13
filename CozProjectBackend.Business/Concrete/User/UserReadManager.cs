fe7c41452d7a6812e837f3aa43740417c7a08dc9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:52:16 +0300

    UserReadManager

---
+++ CozProjectBackend.Business/Concrete/User/UserReadManager.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete
+{
+    public class UserReadManager : IUserReadService
+    {
+        private readonly IUserReadDal _userReadDal;
+        public UserReadManager(IUserReadDal userReadDal)
+        {
+            _userReadDal = userReadDal;
+        }
+        public IDataResult<IQueryable<User>> GetAll()
+        {
+            return new SuccessDataResult<IQueryable<User>>(_userReadDal.GetAll());
+        }
+
+        public async Task<IDataResult<User>> GetByEmailAsync(string email)
+        {
+            User user = await _userReadDal.GetAsync(x => x.Email == email);
+            if (user == null)
+                return new ErrorDataResult<User>();
+            return new SuccessDataResult<User>();
+        }
+
+        public async Task<IDataResult<User>> GetByIdAsync(int id)
+        {
+            User user = await _userReadDal.GetByIdAsync(id);
+            if (user == null)
+                return new ErrorDataResult<User>();
+            return new SuccessDataResult<User>();
+        }
+    }
+}
