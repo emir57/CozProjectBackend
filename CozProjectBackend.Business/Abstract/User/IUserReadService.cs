e7a968a9491e468bee7cf04ed9cf8a8a3c35a9d4
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:47:27 +0300

    IUserReadService

---
+++ CozProjectBackend.Business/Abstract/User/IUserReadService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IUserReadService
+    {
+        Task<IDataResult<User>> GetByIdAsync(int id);
+        Task<IDataResult<User>> GetByEmailAsync(string email);
+        IDataResult<IQueryable<User>> GetAll();
+    }
+}
