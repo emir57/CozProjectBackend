5413f13e640567b0e48d37e2fe49cc2099cc0e82
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 15:59:29 +0300

    IAuthService

---
+++ CozProjectBackend.Business/Abstract/Auth/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract.Auth
+{
+    public interface IAuthService
+    {
+        Task<IDataResult<User>> LoginAsync();
+    }
+}
