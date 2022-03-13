e4cdf023c631e52a3804193fc83c54117bcf3938
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:48:39 +0300

    IUserWriteService

---
+++ CozProjectBackend.Business/Abstract/User/IUserWriteService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IUserWriteService
+    {
+        Task<IResult> AddAsync(User entity);
+        IResult Update(User entity);
+        IResult Delete(User entity);
+        Task<int> SaveAsync();
+    }
+}
