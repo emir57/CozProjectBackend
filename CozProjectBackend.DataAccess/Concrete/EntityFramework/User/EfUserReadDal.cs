ccc1b5b2339f4f6c4edb788822309f041cefc8bc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:45:38 +0300

    EfUserReadDal GetRoles

--- CozProjectBackend.DataAccess/Concrete/EntityFramework/User/EfUserReadDal.cs
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/User/EfUserReadDal.cs
@@ -4,14 +4,33 @@ using CozProjectBackend.DataAccess.Abstract;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
 {
     public class EfUserReadDal : EfReadRepository<User>, IUserReadDal
     {
+        private DbContext _context;
         public EfUserReadDal(DbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public IQueryable<Role> GetRoles(User user)
+        {
+            IQueryable<Role> roles = _context.Set<Role>();
+            IQueryable<UserRole> userRoles = _context.Set<UserRole>();
+            IQueryable<Role> result = from r in roles
+                         join ur in userRoles
+                         on r.Id equals ur.RoleId
+                         where ur.UserId == user.Id
+                         select new Role
+                         {
+                             Id = r.Id,
+                             Name = r.Name
+                         };
+            return result;
         }
     }
 }
