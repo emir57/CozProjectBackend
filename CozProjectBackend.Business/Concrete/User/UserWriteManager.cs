2417f50a55d6412b1f24ae4f5e55a2cdce503b6c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:54:12 +0300

    UserWriteManager

---
+++ CozProjectBackend.Business/Concrete/User/UserWriteManager.cs
@@ -0,0 +1,48 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete
+{
+    public class UserWriteManager : IUserWriteService
+    {
+        private readonly IUserWriteDal _userWriteDal;
+
+        public UserWriteManager(IUserWriteDal userWriteDal)
+        {
+            _userWriteDal = userWriteDal;
+        }
+
+        public async Task<IResult> AddAsync(User entity)
+        {
+            bool result = await _userWriteDal.AddAsync(entity);
+            if (result)
+                return new SuccessResult();
+            return new ErrorResult();
+        }
+
+        public IResult Delete(User entity)
+        {
+            _userWriteDal.Delete(entity);
+            return new SuccessResult();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _userWriteDal.SaveAsync();
+        }
+
+        public IResult Update(User entity)
+        {
+            bool result = _userWriteDal.Update(entity);
+            if (result)
+                return new SuccessResult();
+            return new ErrorResult();
+        }
+    }
+}
