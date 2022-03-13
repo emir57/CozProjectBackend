a8a1f23c15a914fe66145734c2e877b08402e20e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-13 14:42:19 +0300

    IUserReadDal get roles

--- CozProjectBackend.DataAccess/Abstract/User/IUserReadDal.cs
+++ CozProjectBackend.DataAccess/Abstract/User/IUserReadDal.cs
@@ -2,11 +2,13 @@
 using Core.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CozProjectBackend.DataAccess.Abstract
 {
     public interface IUserReadDal:IReadRepository<User>
     {
+        IQueryable<Role> GetRoles(User user);
     }
 }
