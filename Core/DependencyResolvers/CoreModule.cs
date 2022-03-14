7b665c676dfffef931756f612b4b7cf9c15551d6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-14 15:33:27 +0300

    Create CoreModule

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,20 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddSingleton<Stopwatch>();
+            services.AddMemoryCache();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
