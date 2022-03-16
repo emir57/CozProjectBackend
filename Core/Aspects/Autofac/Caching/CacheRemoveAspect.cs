5b7f73a87c3e0556b8a8cf3350d951bad4cef8d7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-16 16:18:17 +0300

    CacheRemoveAspect

---
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,26 @@
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Utilities.IoC;
+using Castle.DynamicProxy;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private string _pattern;
+        private ICacheManager _cache;
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cache = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cache.RemoveByPattern(_pattern);
+        }
+    }
+}
