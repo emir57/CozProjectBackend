e6dd350e39d67f856ddcca35584067366bf9f515
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-16 19:45:46 +0300

    Answer

---
+++ CozProjectBackend.Entities/Concrete/Answer.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Entities.Concrete
+{
+    public class Answer : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsTrue { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
