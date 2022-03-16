40b54de018f79ddd411a0fb9c7c9c83c3ff29352
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-16 19:48:18 +0300

    Question

---
+++ CozProjectBackend.Entities/Concrete/Question.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Entities.Concrete
+{
+    public class Question : IEntity
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
