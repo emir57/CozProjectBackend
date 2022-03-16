4ff5fbc790d320c0b1fb806a85affebd3616534c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-16 16:11:32 +0300

    ValidationAspect

---
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,35 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.AspectMessage;
+using Core.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _loggingType;
+        public ValidationAspect(Type loggingType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(loggingType))
+            {
+                throw new System.Exception(AspectMessages.WrongValidationType);
+            }
+            _loggingType = loggingType;
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            IValidator validator = (IValidator)Activator.CreateInstance(_loggingType);
+            var entityType = _loggingType.BaseType.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(x => x.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
---
+++ Core/Utilities/AspectMessage/AspectMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.AspectMessage
+{
+    public class AspectMessages
+    {
+        public static string WrongValidationType = "Wrong Validation Type";
+        public static string WrongLoggingType = "Wrong Logging Type";
+    }
+}
