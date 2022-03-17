73783cf0aa3fdaab0e7ad53eec6325c78df4eb71
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 14:57:32 +0300

    EfAnswerReadDal and EfAnswerWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Answer/EfAnswerReadDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfAnswerReadDal : EfReadRepository<Answer>, IAnswerReadDal
+    {
+        public EfAnswerReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Answer/EfAnswerWriteDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfAnswerWriteDal : EfWriteRepository<Answer>, IAnswerWriteDal
+    {
+        public EfAnswerWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
