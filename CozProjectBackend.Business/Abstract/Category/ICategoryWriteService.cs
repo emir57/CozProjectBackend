fb62d87c27cc0176754f3bb42ae44ee3f30b25dd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:13:29 +0300

    ICategoryWriteService

---
+++ CozProjectBackend.Business/Abstract/Category/ICategoryWriteService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface ICategoryWriteService
+    {
+        Task<IResult> AddAsync(Category entity);
+        IResult Update(Category entity);
+        IResult Delete(Category entity);
+        Task<int> SaveAsync();
+    }
+}
