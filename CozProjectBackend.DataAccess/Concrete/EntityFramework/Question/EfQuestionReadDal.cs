54c08d37371315af841cbe043e2d78aea22e910a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 14:55:10 +0300

    EfQuestionReadDal and EfQuestionWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Question/EfQuestionReadDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfQuestionReadDal : EfReadRepository<Question>, IQuestionReadDal
+    {
+        public EfQuestionReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Question/EfQuestionWriteDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfQuestionWriteDal : EfWriteRepository<Question>, IQuestionWriteDal
+    {
+        public EfQuestionWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
