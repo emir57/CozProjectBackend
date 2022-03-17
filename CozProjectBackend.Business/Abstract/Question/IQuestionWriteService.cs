8546ea2b8f9dc5044479010016ba4a21f2068b9e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:06:26 +0300

    IQuestionWriteService

---
+++ CozProjectBackend.Business/Abstract/Question/IQuestionWriteService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IQuestionWriteService
+    {
+        Task<IResult> AddAsync(Question question);
+        IResult Update(Question question);
+        IResult Delete(Question question);
+        Task<int> SaveAsync();
+    }
+}
