a8158a07229e30836cec05b622e47e6146aeccc0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:11:32 +0300

    ICategoryReadService

---
+++ CozProjectBackend.Business/Abstract/Category/ICategoryReadService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface ICategoryReadService
+    {
+        Task<IDataResult<List<Category>>> GetListAsync();
+        Task<IDataResult<Category>> GetByIdAsync(int categoryId);
+    }
+}
