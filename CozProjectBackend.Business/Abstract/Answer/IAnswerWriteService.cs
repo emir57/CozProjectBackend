9e5e84bc926bd9f43caed9d6767441db770a5255
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:09:21 +0300

    IAnswerWriteService

---
+++ CozProjectBackend.Business/Abstract/Answer/IAnswerWriteService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IAnswerWriteService
+    {
+        Task<IResult> AddAsync(Answer answer);
+        IResult Update(Answer answer);
+        IResult Delete(Answer answer);
+        Task<int> SaveAsync();
+    }
+}
