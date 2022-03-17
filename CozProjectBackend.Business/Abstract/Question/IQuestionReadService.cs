6bb0c9a1624df3b3d7f9c7072b60eef983d4a860
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:04:07 +0300

    IQuestionReadService

---
+++ CozProjectBackend.Business/Abstract/Question/IQuestionReadService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IQuestionReadService
+    {
+        Task<IDataResult<List<Question>>> GetListByCategoryIdAsync(int categoryId);
+        Task<IDataResult<Question>> GetByIdAsync(int questionId);
+        Task<IDataResult<List<Question>>> GetListAsync();
+    }
+}
