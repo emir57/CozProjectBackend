53785a0af7f391e289779f2568d64852fba5781b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 16:08:19 +0300

    IAnswerReadService

---
+++ CozProjectBackend.Business/Abstract/Answer/IAnswerReadService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IAnswerReadService
+    {
+        Task<IDataResult<List<Answer>>> GetListByQuestionIdAsync(int questionId);
+        Task<IDataResult<List<Answer>>> GetListAsync();
+        Task<IDataResult<Answer>> GetByIdAsync(int answerId);
+    }
+}
