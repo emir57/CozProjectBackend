2d704c6ba0f6f2123781ff0deb9db270440049a7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-17 14:59:11 +0300

    EfCategoryReadDal and EfCategoryWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Category/EfCategoryReadDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryReadDal : EfReadRepository<Category>, ICategoryReadDal
+    {
+        public EfCategoryReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
