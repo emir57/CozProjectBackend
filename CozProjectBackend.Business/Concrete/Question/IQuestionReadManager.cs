f41b38bc2da1f9175de34e99ed26d4d54112b0dd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-18 20:37:27 +0300

    IQuestionReadManager

---
+++ CozProjectBackend.Business/Concrete/Question/IQuestionReadManager.cs
@@ -0,0 +1,40 @@
+﻿using Core.Utilities.Message;
+using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete
+{
+    public class IQuestionReadManager : IQuestionReadService
+    {
+        private readonly IQuestionReadDal _questionReadDal;
+        private readonly ILanguageMessage _language;
+
+        public IQuestionReadManager(IQuestionReadDal questionReadDal, ILanguageMessage language)
+        {
+            _questionReadDal = questionReadDal;
+            _language = language;
+        }
+
+        public async Task<IDataResult<Question>> GetByIdAsync(int questionId)
+        {
+            return new SuccessDataResult<Question>(await _questionReadDal.GetByIdAsync(questionId), _language.SuccessGet);
+        }
+
+        public async Task<IDataResult<List<Question>>> GetListAsync()
+        {
+            return new SuccessDataResult<List<Question>>(await _questionReadDal.GetAll().ToListAsync(), _language.SuccessGet);
+        }
+
+        public async Task<IDataResult<List<Question>>> GetListByCategoryIdAsync(int categoryId)
+        {
+            return new SuccessDataResult<List<Question>>(await _questionReadDal.GetAll(x => x.CategoryId == categoryId).ToListAsync(), _language.SuccessGet);
+        }
+    }
+}
