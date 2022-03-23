101b1b6625386127142a3886151a480192f52efc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 21:08:37 +0300

    RoleValidator

---
+++ CozProjectBackend.Business/Validators/FluentValidation/RoleValidator.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Validators.FluentValidation
+{
+    public class RoleValidator : AbstractValidator<Role>
+    {
+        public RoleValidator()
+        {
+            RuleFor(r => r.Name).NotEmpty();
+            RuleFor(r => r.Name).MaximumLength(30);
+        }
+    }
+}
