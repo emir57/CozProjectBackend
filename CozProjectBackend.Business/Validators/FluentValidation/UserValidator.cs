8c5babea2ca9cce0e290d8a45da0834263f94cea
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 20:48:54 +0300

    UserValidator

---
+++ CozProjectBackend.Business/Validators/FluentValidation/UserValidator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Validators.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).EmailAddress();
+            RuleFor(u => u.Email).MaximumLength(50);
+
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MaximumLength(20);
+
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.LastName).MaximumLength(20);
+        }
+    }
+}
