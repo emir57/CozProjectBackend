cc1e0da37c7b636c69351906f1f90613bb2052af
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 20:51:28 +0300

    UserForRegisterValidator

---
+++ CozProjectBackend.Business/Validators/FluentValidation/UserForRegisterValidator.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Validators.FluentValidation
+{
+    public class UserForRegisterValidator : AbstractValidator<UserForRegisterDto>
+    {
+        public UserForRegisterValidator()
+        {
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).EmailAddress();
+            RuleFor(u => u.Email).MaximumLength(50);
+
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MaximumLength(20);
+
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.LastName).MaximumLength(20);
+
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(6);
+            RuleFor(u => u.Password).MaximumLength(50);
+        }
+    }
+}
