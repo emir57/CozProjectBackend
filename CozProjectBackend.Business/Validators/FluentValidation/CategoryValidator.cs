388b831c868cfb8aea62c7e7a3b014d340eb51dc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 20:46:38 +0300

    CategoryValidator

---
+++ CozProjectBackend.Business/Validators/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,17 @@
+﻿using CozProjectBackend.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Validators.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).MaximumLength(50);
+        }
+    }
+}
