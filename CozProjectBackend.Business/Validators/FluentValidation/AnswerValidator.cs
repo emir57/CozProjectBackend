eb505b08bc375fee27f49a59effa98d4334a0115
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-23 20:45:39 +0300

    AnswerValidator

---
+++ CozProjectBackend.Business/Validators/FluentValidation/AnswerValidator.cs
@@ -0,0 +1,19 @@
+﻿using CozProjectBackend.Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Business.Validators.FluentValidation
+{
+    public class AnswerValidator : AbstractValidator<Answer>
+    {
+        public AnswerValidator()
+        {
+            RuleFor(a => a.QuestionId).NotEmpty();
+            RuleFor(a => a.Content).NotEmpty();
+            RuleFor(a => a.Content).MaximumLength(150);
+            RuleFor(a => a.IsTrue).NotNull();
+        }
+    }
+}
