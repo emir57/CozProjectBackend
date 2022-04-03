db1ef807209d49ddccbdbfb3ae8523324c0ff90f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 14:43:48 +0300

    Create CategoriesController

---
+++ CozProjectBackend.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using CozProjectBackend.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryReadService _categoryReadService;
+        private readonly ICategoryWriteService _categoryWriteService;
+
+        public CategoriesController(ICategoryWriteService categoryWriteService, ICategoryReadService categoryReadService)
+        {
+            _categoryWriteService = categoryWriteService;
+            _categoryReadService = categoryReadService;
+        }
+    }
+}
