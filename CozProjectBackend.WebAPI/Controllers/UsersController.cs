fd4d99c019d9fe0807065ebe46c4384c60f051de
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 15:19:54 +0300

    Create UsersController

---
+++ CozProjectBackend.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using CozProjectBackend.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserReadService _userReadService;
+        private readonly IUserWriteService _userWriteService;
+        public UsersController(IUserReadService userReadService, IUserWriteService userWriteService)
+        {
+            _userReadService = userReadService;
+            _userWriteService = userWriteService;
+        }
+    }
+}
