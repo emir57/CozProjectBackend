31cc4c800075d3b1630e6158c79fad7e1e7ceb9e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 15:13:41 +0300

    Create QuestionsController

---
+++ CozProjectBackend.WebAPI/Controllers/QuestionsController.cs
@@ -0,0 +1,23 @@
+﻿using CozProjectBackend.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly IQuestionWriteService _questionWriteService;
+        private readonly IQuestionReadService _questionReadService;
+        public QuestionsController(IQuestionWriteService questionWriteService, IQuestionReadService questionReadService)
+        {
+            _questionWriteService = questionWriteService;
+            _questionReadService = questionReadService;
+        }
+    }
+}
