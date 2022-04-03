dd28e9356aebd3f2885606104f9d86a91cf8aca1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-03 14:34:34 +0300

    Create AnswersController

---
+++ CozProjectBackend.WebAPI/Controllers/AnswersController.cs
@@ -0,0 +1,23 @@
+﻿using CozProjectBackend.DataAccess.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    public class AnswersController : Controller
+    {
+        private readonly IAnswerReadDal _answerReadDal;
+        private readonly IAnswerWriteDal _answerWriteDal;
+
+        public AnswersController(IAnswerReadDal answerReadDal, IAnswerWriteDal answerWriteDal)
+        {
+            _answerReadDal = answerReadDal;
+            _answerWriteDal = answerWriteDal;
+        }
+
+        
+    }
+}
