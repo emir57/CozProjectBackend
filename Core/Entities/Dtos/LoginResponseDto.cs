3d6fc058f98d2e1ced1fe5e010d2a5ffb47155dd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-13 17:05:52 +0300

    LoginResponseDto

---
+++ Core/Entities/Dtos/LoginResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Core.Utilities.Security.JWT;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Dtos
+{
+    public class LoginResponseDto : IDto
+    {
+        public LoginedUserDto User { get; set; }
+        public AccessToken Token { get; set; }
+    }
+}
