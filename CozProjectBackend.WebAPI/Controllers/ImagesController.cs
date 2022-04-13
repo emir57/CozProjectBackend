c13177f8ccb3685f9cd757aa4e5a5ac72371026e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 00:09:04 +0300

    upload image test

---
+++ CozProjectBackend.WebAPI/Controllers/ImagesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            string ex = Path.GetExtension(file.FileName);
+            return Ok();
+        }
+    }
+}
