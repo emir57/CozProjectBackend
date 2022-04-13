6642313ceef7b270dfce1e6843822434fd4b76d1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-13 17:10:08 +0300

    Create CoreLayerProfile

---
+++ Core/Entities/MapperProfiles/CoreLayerProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Entities.Concrete;
+using Core.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.MapperProfiles
+{
+    public class CoreLayerProfile : Profile
+    {
+        public CoreLayerProfile()
+        {
+            CreateMap<User, LoginedUserDto>();
+            CreateMap<LoginedUserDto, User>();
+        }
+    }
+}
