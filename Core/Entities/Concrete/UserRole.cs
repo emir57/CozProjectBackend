b351db198617aad2b368e4c14f9ee11ce3abc745
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 00:39:48 +0300

    UserRole

--- Core/Entities/Concrete/UserRole.cs
+++ Core/Entities/Concrete/UserRole.cs
@@ -9,7 +9,8 @@ namespace Core.Entities.Concrete
         public int Id { get; set; }
         public int UserId { get; set; }
         public int RoleId { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
     }
 }
