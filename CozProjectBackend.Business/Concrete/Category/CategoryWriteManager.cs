17f283e1dfe7c75c97c5e9ffdac3a91d6cfad9ee
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 22:25:28 +0300

    CheckCategoryName Rule

--- CozProjectBackend.Business/Concrete/Category/CategoryWriteManager.cs
+++ CozProjectBackend.Business/Concrete/Category/CategoryWriteManager.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Message;
 using Core.Utilities.Result;
 using CozProjectBackend.Business.Abstract;
@@ -7,6 +8,7 @@ using CozProjectBackend.DataAccess.Abstract;
 using CozProjectBackend.Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +18,37 @@ namespace CozProjectBackend.Business.Concrete
     {
         private readonly ICategoryWriteDal _categoryWriteDal;
         private readonly ILanguageMessage _language;
+        private readonly ICategoryReadService _categoryReadService;
 
-        public CategoryWriteManager(ILanguageMessage language, ICategoryWriteDal categoryWriteDal)
+        public CategoryWriteManager(ILanguageMessage language, ICategoryWriteDal categoryWriteDal, ICategoryReadService categoryReadService)
         {
             _language = language;
             _categoryWriteDal = categoryWriteDal;
+            _categoryReadService = categoryReadService;
         }
         [ValidationAspect(typeof(CategoryValidator))]
         public async Task<IResult> AddAsync(Category entity)
         {
-            bool result = await _categoryWriteDal.AddAsync(entity);
-            if (result)
+            var result = BusinessRules.Run(
+                await CheckCategoryName(entity));
+            if(result != null)
+            {
+                return result;
+            }
+            bool addResult = await _categoryWriteDal.AddAsync(entity);
+            if (addResult)
                 return new SuccessResult(_language.SuccessAdd);
             return new ErrorResult(_language.FailureAdd);
+        }
+
+        private async Task<IResult> CheckCategoryName(Category entity)
+        {
+            var categories = await _categoryReadService.GetListAsync();
+            if (categories.Data.Any(x=>x.Name == entity.Name))
+            {
+                return new ErrorResult("Böyle bir kategori zaten var");
+            }
+            return new SuccessResult();
         }
 
         public IResult Delete(Category entity)
