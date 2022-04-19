d75b318c7357a7cef0cc4ac20d7f1ebf5937b9f1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:42:46 +0300

    EfCategoryCompleteReadDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/CategoryComplete/EfCategoryCompleteReadDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryCompleteReadDal : EfReadRepository<CategoryComplete>, ICategoryCompleteReadDal
+    {
+        public EfCategoryCompleteReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
