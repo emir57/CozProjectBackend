2f1fd8b26e8012eed19237f407e5174834041241
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:58:44 +0300

    EfQuestionCompleteReadDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/QuestionComplete/EfQuestionCompleteReadDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfQuestionCompleteReadDal : EfReadRepository<QuestionComplete>
+    {
+        public EfQuestionCompleteReadDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
