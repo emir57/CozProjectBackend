b46b507cedc952338a9187b0ebb5a2aae054a4d6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 19:01:09 +0300

    IQuestionCompleteWriteService

---
+++ CozProjectBackend.Business/Abstract/QuestionComplete/IQuestionCompleteWriteService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract
+{
+    public interface IQuestionCompleteWriteService
+    {
+        Task<IResult> AddAsync(QuestionComplete questionComplete);
+        IResult Delete(QuestionComplete questionComplete);
+    }
+}
