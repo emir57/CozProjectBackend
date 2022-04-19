de692d258d30d6f23125ba2d2402a4f4efbe282c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:52:58 +0300

    Create QuestionComplete Entity

---
+++ CozProjectBackend.Entities/Concrete/QuestionComplete.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Entities.Concrete
+{
+    public class QuestionComplete : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int QuestionId { get; set; }
+        public bool Result { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
