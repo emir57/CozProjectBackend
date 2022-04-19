c1685451a87a55aaefc07e50cb54a18bf009cf41
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 19:06:01 +0300

    QuestionCompleteReadManager

---
+++ CozProjectBackend.Business/Concrete/QuestionComplete/QuestionCompleteReadManager.cs
@@ -0,0 +1,32 @@
+﻿using Core.Utilities.Message;
+using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete.QuestionComplete
+{
+    public class QuestionCompleteReadManager : IQuestionCompleteReadService
+    {
+        private readonly ILanguageMessage _language;
+        private readonly IQuestionCompleteReadDal _questionCompleteReadDal;
+
+        public QuestionCompleteReadManager(IQuestionCompleteReadDal questionCompleteReadDal, ILanguageMessage language)
+        {
+            _questionCompleteReadDal = questionCompleteReadDal;
+            _language = language;
+        }
+        public async Task<IDataResult<bool>> CheckQuestionCompleteAsync(int userId, int questionId)
+        {
+            var categoryComplete = await _questionCompleteReadDal.GetAsync(x => x.UserId == userId && x.QuestionId == questionId);
+            if (categoryComplete == null)
+            {
+                return new SuccessDataResult<bool>(true);
+            }
+            return new SuccessDataResult<bool>(false);
+        }
+    }
+}
