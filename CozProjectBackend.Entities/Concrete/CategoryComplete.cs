5b349e0b21eb27647742c243f5b4f6660d45c091
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:27:52 +0300

    CategoryComplete Entity

---
+++ CozProjectBackend.Entities/Concrete/CategoryComplete.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Entities.Concrete
+{
+    public class CategoryComplete : IEntity
+    {
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public int Id { get; set ; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
