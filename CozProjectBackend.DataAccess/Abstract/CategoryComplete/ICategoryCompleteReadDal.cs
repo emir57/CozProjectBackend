df6615c1d5c9b26897090eebb20344dcb1cc7888
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:29:48 +0300

    ICategoryCompleteReadDal and ICategoryCompleteWriteDal

---
+++ CozProjectBackend.DataAccess/Abstract/CategoryComplete/ICategoryCompleteReadDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess;
+using CozProjectBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Abstract
+{
+    public interface ICategoryCompleteReadDal : IReadRepository<CategoryComplete>
+    {
+    }
+}
---
+++ CozProjectBackend.DataAccess/Abstract/CategoryComplete/ICategoryCompleteWriteDal.cs
@@ -0,0 +1,9 @@
+﻿using Core.DataAccess;
+using CozProjectBackend.Entities.Concrete;
+
+namespace CozProjectBackend.DataAccess.Abstract
+{
+    public interface ICategoryCompleteWriteDal : IWriteRepository<CategoryComplete>
+    {
+    }
+}
