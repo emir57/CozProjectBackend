1642c94a14df440a2855b814464344f61df10229
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:14:04 +0300

    GetAllWithAnswers

--- CozProjectBackend.DataAccess/Concrete/EntityFramework/Question/EfQuestionReadDal.cs
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Question/EfQuestionReadDal.cs
@@ -5,13 +5,32 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
 {
     public class EfQuestionReadDal : EfReadRepository<Question>, IQuestionReadDal
     {
+        private readonly DbContext _context;
         public EfQuestionReadDal(DbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<List<Question>> GetAllWithAnswers()
+        {
+            var result = from q in _context.Set<Question>()
+                         select new Question
+                         {
+                             Id = q.Id,
+                             CategoryId = q.CategoryId,
+                             TeacherId = q.TeacherId,
+                             Score = q.Score,
+                             Content = q.Content,
+                             Answers = _context.Set<Answer>().Where(x => x.QuestionId == q.Id).ToList(),
+                         };
+            return await result.ToListAsync();
         }
     }
 }
