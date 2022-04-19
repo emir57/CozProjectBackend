4bdeecef5b1773e44c44f49385a9d27e829dc297
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:32:09 +0300

    CheckCategoryCompleteAsync

---
+++ CozProjectBackend.Business/Abstract/CategoryComplete/ICategoryCompleteReadService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Utilities.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Abstract.CategoryComplete
+{
+    public interface ICategoryCompleteReadService
+    {
+        Task<IDataResult<bool>> CheckCategoryCompleteAsync(int userId, int categoryId);
+    }
+}
