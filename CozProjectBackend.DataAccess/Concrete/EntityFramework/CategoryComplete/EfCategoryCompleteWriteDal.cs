36a895040689825e8abe2c1d98af648ea7c616e6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:43:58 +0300

    EfCategoryCompleteWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/CategoryComplete/EfCategoryCompleteWriteDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.DataAccess.Abstract;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfCategoryCompleteWriteDal : EfWriteRepository<CategoryComplete>, ICategoryCompleteWriteDal
+    {
+        public EfCategoryCompleteWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
