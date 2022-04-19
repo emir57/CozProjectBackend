2d3c43c93436796b4aceee5786cc6a318f40c3dd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:57:59 +0300

    EfQuestionCompleteWriteDal

---
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/QuestionComplete/EfQuestionCompleteWriteDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.EntityFramework;
+using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
+{
+    public class EfQuestionCompleteWriteDal : EfWriteRepository<QuestionComplete>
+    {
+        public EfQuestionCompleteWriteDal(DbContext context) : base(context)
+        {
+        }
+    }
+}
