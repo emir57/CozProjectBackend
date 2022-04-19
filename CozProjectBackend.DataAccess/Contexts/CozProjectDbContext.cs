40636091137f79789f81d1e0075c94bf2ea4f455
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:30:28 +0300

    DbContext CategoryCompletes

--- CozProjectBackend.DataAccess/Contexts/CozProjectDbContext.cs
+++ CozProjectBackend.DataAccess/Contexts/CozProjectDbContext.cs
@@ -39,5 +39,6 @@ namespace CozProjectBackend.DataAccess.Contexts
         public DbSet<Question> Questions { get; set; }
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryComplete> CategoryCompletes { get; set; }
     }
 }
