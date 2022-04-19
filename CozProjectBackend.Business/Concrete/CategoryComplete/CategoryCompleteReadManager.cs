e12d38f8910316c2d208f63b4fbbed9e8f8c8143
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:36:15 +0300

    CheckCategoryCompleteAsync in Manager

---
+++ CozProjectBackend.Business/Concrete/CategoryComplete/CategoryCompleteReadManager.cs
@@ -0,0 +1,30 @@
+﻿using Core.Utilities.Result;
+using CozProjectBackend.Business.Abstract;
+using CozProjectBackend.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.Business.Concrete.CategoryComplete
+{
+    public class CategoryCompleteReadManager : ICategoryCompleteReadService
+    {
+        private readonly ICategoryCompleteReadDal _categoryCompleteReadDal;
+
+        public CategoryCompleteReadManager(ICategoryCompleteReadDal categoryCompleteReadDal)
+        {
+            _categoryCompleteReadDal = categoryCompleteReadDal;
+        }
+
+        public async Task<IDataResult<bool>> CheckCategoryCompleteAsync(int userId, int categoryId)
+        {
+            var categoryComplete = await _categoryCompleteReadDal.GetAsync(x => x.UserId == userId && x.CategoryId == categoryId);
+            if (categoryComplete == null)
+            {
+                return new SuccessDataResult<bool>(true);
+            }
+            return new SuccessDataResult<bool>(false);
+        }
+    }
+}
