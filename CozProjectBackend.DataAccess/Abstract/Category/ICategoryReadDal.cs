25cce9117c3ea54c1ac8a85eb1ee1f0b4020b07a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:57:02 +0300

    GetCategoriesWithComplete Dal

--- CozProjectBackend.DataAccess/Abstract/Category/ICategoryReadDal.cs
+++ CozProjectBackend.DataAccess/Abstract/Category/ICategoryReadDal.cs
@@ -3,10 +3,12 @@ using CozProjectBackend.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CozProjectBackend.DataAccess.Abstract
 {
     public interface ICategoryReadDal : IReadRepository<Category>
     {
+        Task<List<Category>> GetCategoriesWithComplete(int userId);
     }
 }
