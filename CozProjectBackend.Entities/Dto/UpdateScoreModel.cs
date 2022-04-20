6490f9a61d92bb93f4c2986d1d397cc44a32cce6
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-20 16:38:01 +0300

    UpdateScoreModel

---
+++ CozProjectBackend.Entities/Dto/UpdateScoreModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CozProjectBackend.Entities.Dto
+{
+    public class UpdateScoreModel
+    {
+        public int UserId { get; set; }
+        public int QuestionId { get; set; }
+        public bool Result { get; set; }
+        public int Score { get; set; }
+    }
+}
