9fefbe5df05430478e15bd76005f7acb1a636770
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-04 14:00:54 +0300

    UserResetPasswordDto Add Email

--- Core/Entities/Dtos/UserResetPasswordDto.cs
+++ Core/Entities/Dtos/UserResetPasswordDto.cs
@@ -6,6 +6,7 @@ namespace Core.Entities.Dtos
 {
     public class UserResetPasswordDto : IDto
     {
+        public string Email { get; set; }
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
     }
