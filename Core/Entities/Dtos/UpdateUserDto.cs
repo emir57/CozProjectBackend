e58e3d8147c74e843ec35f9b5f645baabdfe0c1b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-04 13:40:48 +0300

    Create UpdateUserDto

---
+++ Core/Entities/Dtos/UpdateUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Dtos
+{
+    public class UpdateUserDto : IDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
