3ff5a481afb34cda20a279e13b24ddc1f8c74627
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-14 15:32:32 +0300

    EfRoleReadDal IsInRole complete

--- CozProjectBackend.DataAccess/Concrete/EntityFramework/Role/EfRoleReadDal.cs
+++ CozProjectBackend.DataAccess/Concrete/EntityFramework/Role/EfRoleReadDal.cs
@@ -5,13 +5,22 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CozProjectBackend.DataAccess.Concrete.EntityFramework
 {
     public class EfRoleReadDal : EfReadRepository<Role>, IRoleReadDal
     {
+        private readonly DbContext _context;
         public EfRoleReadDal(DbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public async Task<bool> IsInRole(User user, Role role)
+        {
+            var userRole = await _context.Set<UserRole>().SingleOrDefaultAsync(x => x.UserId == user.Id && x.RoleId == role.Id);
+            return userRole == null ? false : true;
         }
     }
 }
