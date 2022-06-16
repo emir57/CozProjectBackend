ab3c11b3ee90fb5a8be54227c3a7b5393ce0ce3a
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:23:40 +0300

    FluentEntity AddParameter expression

---
+++ Core/Entities/FluentEntities/FluentEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Entities.FluentEntities
+{
+    public class FluentEntity<T>
+        where T : class, IEntity, new()
+    {
+        private T entity;
+        public FluentEntity()
+        {
+            entity = new T();
+        }
+        public FluentEntity<T> AddParameter(Expression<Func<T, object>> exp, object value)
+        {
+
+            return this;
+        }
+    }
+}
---
+++ CozProjectBackend.WebAPI/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete;
+using Core.Entities.FluentEntities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CozProjectBackend.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            var user = new FluentEntity<User>()
+                .AddParameter(x=>x.);
+            return Ok();
+        }
+    }
+}
