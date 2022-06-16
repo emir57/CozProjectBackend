98f802d4eb2d154e06dc471a4ff299789b708c1b
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 14:46:13 +0300

    IFluentEntity -> AddParameter

--- Core/Entities/FluentEntities/IFluentEntity.cs
+++ Core/Entities/FluentEntities/IFluentEntity.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Core.Entities.FluentEntities
 {
-    public interface IFluentEntity
+    public interface IFluentEntity<T>
+        where T : class, new()
     {
+        IFluentEntity<T> AddParameter<P>(Expression<Func<T, P>> exp, object value);
     }
 }
