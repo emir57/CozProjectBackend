7ea4b5f78e9a597306f9905f63e11d2fd665c722
emir57 <emir.gurbuz06@hotmail.com>
2022-06-16 17:30:12 +0300

    Update FEntity

--- Core/CrossCuttingConcerns/FEntity/FluentEntityBase.cs
+++ Core/CrossCuttingConcerns/FEntity/FluentEntityBase.cs
@@ -19,17 +19,39 @@ namespace FluentEntity_ConsoleApp.FEntity
         {
             _entity = entity;
         }
+
+        /// <summary>
+        /// assigns a value to the selected property
+        /// </summary>
+        /// <typeparam name="P">Property Type</typeparam>
+        /// <param name="exp">Expression</param>
+        /// <param name="value">Value</param>
+        /// <returns></returns>
         public virtual FluentEntityBase<T> AddParameter<P>(Expression<Func<T, P>> exp, object value)
         {
             string propertyName = (exp.Body as MemberExpression).Member.Name;
             SetProperty(propertyName, value);
             return this;
         }
+
+        /// <summary>
+        /// assigns value by property name
+        /// </summary>
+        /// <param name="propertyName">Property Name</param>
+        /// <param name="value">Value</param>
+        /// <returns></returns>
         public virtual FluentEntityBase<T> AddParameter(string propertyName, object value)
         {
             SetProperty(propertyName, value);
             return this;
         }
+
+        /// <summary>
+        /// assigns values to all properties of the selected type
+        /// </summary>
+        /// <typeparam name="P"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public virtual FluentEntityBase<T> AddParameters<P>(object value)
         {
             PropertyInfo[] propertyInfos = _entity.GetType().GetProperties();
@@ -39,14 +61,28 @@ namespace FluentEntity_ConsoleApp.FEntity
             return this;
         }
 
+        /// <summary>
+        /// returns the entity with values
+        /// </summary>
+        /// <returns></returns>
         public T GetEntity() => _entity;
 
+        /// <summary>
+        /// assigns value to property
+        /// </summary>
+        /// <param name="propertyName">Property Name</param>
+        /// <param name="value">Value</param>
         protected virtual void SetProperty(string propertyName, object value)
         {
             PropertyInfo propertyInfo = _entity.GetType().GetProperty(propertyName);
             CheckExceptions(propertyInfo);
             propertyInfo.SetValue(_entity, value);
         }
+        /// <summary>
+        /// checks for errors while assigning value to property
+        /// </summary>
+        /// <param name="propertyInfo">Property Info</param>
+        /// <exception cref="PropertyNotFoundFluentEntityException"></exception>
         protected virtual void CheckExceptions(PropertyInfo propertyInfo)
         {
             if (propertyInfo == null) throw new PropertyNotFoundFluentEntityException();
