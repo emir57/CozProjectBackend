8cddfb6da09d504d74984fb0071c16b90cf932ff
emir57 <emir.gurbuz06@hotmail.com>
2022-09-13 13:56:58 +0300

    Create QuestionCompleteConfig

---
+++ CozProjectBackend.DataAccess/Config/QuestionCompleteConfig.cs
@@ -0,0 +1,39 @@
+﻿using CozProjectBackend.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CozProjectBackend.DataAccess.Config
+{
+    public sealed class QuestionCompleteConfig : IEntityTypeConfiguration<QuestionComplete>
+    {
+        public void Configure(EntityTypeBuilder<QuestionComplete> builder)
+        {
+            builder.ToTable("QuestionCompletes")
+                .HasKey(q => q.Id);
+
+            builder.HasOne(q => q.User);
+            builder.HasOne(q => q.Question);
+
+            builder.Property(q => q.Id)
+                .HasColumnName("Id");
+
+            builder.Property(q => q.UserId)
+                .IsRequired()
+                .HasColumnName("UserId");
+
+            builder.Property(q => q.QuestionId)
+                .IsRequired()
+                .HasColumnName("QuestionId");
+
+            builder.Property(q => q.Result)
+                .HasColumnName("Result");
+
+            builder.Property(q => q.CreatedDate)
+                .HasColumnName("CreatedDate");
+            builder.Property(q => q.UpdatedDate)
+                .HasColumnName("UpdatedDate");
+            builder.Property(q => q.DeletedDate)
+                .HasColumnName("DeletedDate");
+        }
+    }
+}
