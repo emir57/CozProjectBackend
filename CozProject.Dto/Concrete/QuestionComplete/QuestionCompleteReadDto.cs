e5919cad3eb52c337f64cd0f655aa79b832780fe
emir57 <emir.gurbuz06@hotmail.com>
2022-09-16 18:21:21 +0300

    Create QuestionCompleteWriteDto And QuestionCompleteReadDto

---
+++ CozProject.Dto/Concrete/QuestionComplete/QuestionCompleteReadDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Dtos.Abstract;
+using System;
+
+namespace CozProject.Dto.Concrete.QuestionComplete
+{
+    public sealed class QuestionCompleteReadDto : IReadDto
+    {
+        public int Id { get; set; }
+        public UserReadDto User { get; set; }
+        public CategoryReadDto Category { get; set; }
+        public bool? Result { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ CozProject.Dto/Concrete/QuestionComplete/QuestionCompleteWriteDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Dtos.Abstract;
+
+namespace CozProject.Dto.Concrete.QuestionComplete
+{
+    public sealed class QuestionCompleteWriteDto : IWriteDto
+    {
+        public int UserId { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
