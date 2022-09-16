42b145b01caedd95de4ef36f41b3e6fefd47f891
emir57 <emir.gurbuz06@hotmail.com>
2022-09-16 18:24:27 +0300

    Create CategoryCompleteWriteDto And CategoryCompleteReadDto

---
+++ CozProject.Dto/Concrete/CategoryComplete/CategoryCompleteReadDto.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Dtos.Abstract;
+using System;
+
+namespace CozProject.Dto.Concrete
+{
+    public sealed class CategoryCompleteReadDto : IReadDto
+    {
+        public int Id { get; set; }
+        public UserReadDto User { get; set; }
+        public CategoryReadDto Category { get; set; }
+
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ CozProject.Dto/Concrete/CategoryComplete/CategoryCompleteWriteDto.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Dtos.Abstract;
+
+namespace CozProject.Dto.Concrete
+{
+    public sealed class CategoryCompleteWriteDto : IWriteDto
+    {
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
