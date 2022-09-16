926dd9cb02eae3f0543bb37f90b3a7abb55f6100
emir57 <emir.gurbuz06@hotmail.com>
2022-09-16 18:16:20 +0300

    Create QuestionWriteDto And QuestionReadDto

---
+++ CozProject.Dto/Concrete/Question/QuestionReadDto.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Dtos.Abstract;
+using System;
+using System.Collections.Generic;
+
+namespace CozProject.Dto.Concrete
+{
+    public sealed class QuestionReadDto : IReadDto
+    {
+        public int Id { get; set; }
+        public CategoryReadDto Category { get; set; }
+        public string Content { get; set; }
+        public int Score { get; set; }
+        public bool? Result { get; set; }
+        public UserReadDto User { get; set; }
+        public ICollection<AnswerReadDto> Answers { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ CozProject.Dto/Concrete/Question/QuestionWriteDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Dtos.Abstract;
+
+namespace CozProject.Dto.Concrete
+{
+    public sealed class QuestionWriteDto : IWriteDto
+    {
+        public string Content { get; set; }
+        public int Score { get; set; }
+        public int TeacherId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
