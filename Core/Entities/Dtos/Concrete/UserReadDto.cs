7fe4082b3bdec0ba0d291382293ac28344a1aeb3
emir57 <emir.gurbuz06@hotmail.com>
2022-09-16 18:26:44 +0300

    Create UserReadDto

---
+++ Core/Entities/Dtos/Concrete/UserReadDto.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Dtos.Abstract;
+using System;
+
+namespace Core.Entities.Dtos.Concrete
+{
+    public sealed class UserReadDto : IReadDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Score { get; set; }
+        public string ProfilePhotoUrl { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
