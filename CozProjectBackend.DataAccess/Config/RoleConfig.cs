5200b1bd3c7231121b5604c276f23b7397d5a1b9
emir57 <emir.gurbuz06@hotmail.com>
2022-09-16 14:48:18 +0300

    Create RoleConfig

---
+++ CozProjectBackend.DataAccess/Config/RoleConfig.cs
@@ -0,0 +1,30 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CozProjectBackend.DataAccess.Config
+{
+    public sealed class RoleConfig : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles")
+                .HasKey(r => r.Id);
+
+            builder.Property(r => r.Id)
+                .HasColumnName("Id");
+
+            builder.Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("Name");
+
+            builder.Property(c => c.CreatedDate)
+                .HasColumnName("CreatedDate");
+            builder.Property(c => c.UpdatedDate)
+                .HasColumnName("UpdatedDate");
+            builder.Property(c => c.DeletedDate)
+                .HasColumnName("DeletedDate");
+        }
+    }
+}
