c3819d13daf6e164da22be17af6ca680dedc084f
emir57 <emir.gurbuz06@hotmail.com>
2022-10-18 20:21:01 +0300

    Created IWriteBaseService and IReadBaseService

---
+++ CozProject.Business/Abstract/IReadBaseService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Utilities.Result;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CozProject.Business.Abstract;
+
+public interface IReadBaseService<TEntity, TWriteDto, TReadDto>
+{
+    Task<IDataResult<List<TReadDto>>> GetListAsync();
+    Task<IDataResult<TReadDto>> GetByIdAsync(int id);
+}
---
+++ CozProject.Business/Abstract/IWriteBaseService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Result;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CozProject.Business.Abstract;
+
+public interface IWriteBaseService<TEntity, TWriteDto, TReadDto>
+{
+    Task<IResult> AddAsync(TWriteDto writeDto);
+    IResult Update(TWriteDto writeDto);
+    IResult UpdateRange(List<TWriteDto> writeDtos);
+    IResult Delete(TWriteDto writeDto);
+    IResult DeleteRange(List<TWriteDto> writeDtos);
+    Task<IResult> AddRangeAsync(List<TWriteDto> writeDtos);
+    Task<int> SaveAsync();
+}
