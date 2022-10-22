0b4bee4459f1905a14c0b761dd3302e5d7029a48
emir57 <emir.gurbuz06@hotmail.com>
2022-10-22 19:51:39 +0300

    IAnswerReadService Implement IReadBaseService

--- CozProject.Business/Abstract/Answer/IAnswerReadService.cs
+++ CozProject.Business/Abstract/Answer/IAnswerReadService.cs
@@ -1,14 +1,12 @@
 ﻿using Core.Utilities.Result;
+using CozProject.Dto.Concrete;
 using CozProject.Entities.Concrete;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace CozProject.Business.Abstract
+namespace CozProject.Business.Abstract;
+
+public interface IAnswerReadService : IReadBaseService<Answer, AnswerWriteDto, AnswerReadDto>
 {
-    public interface IAnswerReadService
-    {
-        Task<IDataResult<List<Answer>>> GetListByQuestionIdAsync(int questionId);
-        Task<IDataResult<List<Answer>>> GetListAsync();
-        Task<IDataResult<Answer>> GetByIdAsync(int answerId);
-    }
+    Task<IDataResult<List<Answer>>> GetListByQuestionIdAsync(int questionId);
 }
