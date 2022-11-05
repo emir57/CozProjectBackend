6cc45a36d7473311d4d07d4e8946aef698c6581b
emir57 <emir.gurbuz06@hotmail.com>
2022-11-05 23:08:13 +0300

    Refactoring AnswerReadManager

--- CozProject.Business/Concrete/Answer/AnswerReadManager.cs
+++ CozProject.Business/Concrete/Answer/AnswerReadManager.cs
@@ -9,17 +9,16 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace CozProject.Business.Concrete
-{
-    public class AnswerReadManager : ReadBaseService<Answer, AnswerWriteDto, AnswerReadDto>, IAnswerReadService
-    {
-        public AnswerReadManager(IAnswerReadDal _answerReadDal, IMapper mapper, ILanguageMessage languageMessage) : base(_answerReadDal, mapper, languageMessage)
-        {
-        }
+namespace CozProject.Business.Concrete;
 
-        public async Task<IDataResult<List<Answer>>> GetListByQuestionIdAsync(int questionId)
-        {
-            return new SuccessDataResult<List<Answer>>(await ReadRepository.GetAll(x => x.QuestionId == questionId).ToListAsync(), LanguageMessage.SuccessList);
-        }
+public class AnswerReadManager : ReadBaseService<Answer, AnswerWriteDto, AnswerReadDto>, IAnswerReadService
+{
+    public AnswerReadManager(IAnswerReadDal answerReadDal, IMapper mapper, ILanguageMessage languageMessage) : base(answerReadDal, mapper, languageMessage)
+    {
+    }
+
+    public async Task<IDataResult<List<Answer>>> GetListByQuestionIdAsync(int questionId)
+    {
+        return new SuccessDataResult<List<Answer>>(await ReadRepository.GetAll(x => x.QuestionId == questionId).ToListAsync(), LanguageMessage.SuccessList);
     }
 }
