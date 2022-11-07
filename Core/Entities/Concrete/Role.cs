5e30fcfc1be02fce05089cdc285a8a68fef61005
emir57 <emir.gurbuz06@hotmail.com>
2022-11-07 21:57:16 +0300

    Refactoring core entities

--- Core/Entities/Concrete/Role.cs
+++ Core/Entities/Concrete/Role.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Core.Entities.Concrete
+namespace Core.Entities.Concrete;
+
+public class Role : IEntity
 {
-    public class Role : IEntity
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+
+    public ICollection<UserRole> UserRoles { get; set; }
+
+    public Role()
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-        public DateTime? DeletedDate { get; set; }
+        UserRoles = new HashSet<UserRole>();
+    }
 
-        public ICollection<UserRole> UserRoles { get; set; }
-
-        public Role()
-        {
-            UserRoles = new HashSet<UserRole>();
-        }
-
-        public Role(int id, string name, DateTime? createdDate) : this()
-        {
-            Id = id;
-            Name = name;
-            CreatedDate = createdDate;
-        }
+    public Role(int id, string name, DateTime? createdDate) : this()
+    {
+        Id = id;
+        Name = name;
+        CreatedDate = createdDate;
     }
 }
--- Core/Entities/Concrete/UserRole.cs
+++ Core/Entities/Concrete/UserRole.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Core.Entities.Concrete
+namespace Core.Entities.Concrete;
+
+public class UserRole : IEntity
 {
-    public class UserRole : IEntity
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+
+    [ForeignKey("UserId")]
+    public User User { get; set; }
+
+    [ForeignKey("RoleId")]
+    public Role Role { get; set; }
+    public UserRole()
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        public int RoleId { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-        public DateTime? DeletedDate { get; set; }
+        User = new User();
+        Role = new Role();
+    }
 
-        [ForeignKey("UserId")]
-        public User User { get; set; }
-
-        [ForeignKey("RoleId")]
-        public Role Role { get; set; }
-        public UserRole()
-        {
-            User = new User();
-            Role = new Role();
-        }
-
-        public UserRole(int id, int userId, int roleId, DateTime? createdDate)
-        {
-            Id = id;
-            UserId = userId;
-            RoleId = roleId;
-            CreatedDate = createdDate;
-        }
+    public UserRole(int id, int userId, int roleId, DateTime? createdDate)
+    {
+        Id = id;
+        UserId = userId;
+        RoleId = roleId;
+        CreatedDate = createdDate;
     }
 }
