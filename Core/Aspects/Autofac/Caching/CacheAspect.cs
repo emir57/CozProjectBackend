c890e9a993933b2f7992293757a3aed59517c241
emir57 <emir.gurbuz06@hotmail.com>
2022-11-09 20:11:53 +0300

    Refactoring cache aspects.

--- Core/Aspects/Autofac/Caching/CacheAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -1,36 +1,32 @@
-﻿using Core.CrossCuttingConcerns.Caching;
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
 using Core.Utilities.Interceptors;
 using Core.Utilities.IoC;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Microsoft.Extensions.DependencyInjection;
-using Castle.DynamicProxy;
 using System.Linq;
 
-namespace Core.Aspects.Autofac.Caching
+namespace Core.Aspects.Autofac.Caching;
+
+public class CacheAspect : MethodInterception
 {
-    public class CacheAspect : MethodInterception
+    private int _duration;
+    private ICacheManager _cache;
+    public CacheAspect(int duration = 60)
     {
-        private int _duration;
-        private ICacheManager _cache;
-        public CacheAspect(int duration = 60)
+        _duration = duration;
+        _cache = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+    }
+    public override void Intercept(IInvocation invocation)
+    {
+        string methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
+        var arguments = invocation.Arguments;
+        string key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
+        if (_cache.IsAdd(key))
         {
-            _duration = duration;
-            _cache = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+            invocation.ReturnValue = _cache.Get(key);
+            return;
         }
-        public override void Intercept(IInvocation invocation)
-        {
-            string methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
-            var arguments = invocation.Arguments;
-            string key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
-            if (_cache.IsAdd(key))
-            {
-                invocation.ReturnValue = _cache.Get(key);
-                return;
-            }
-            invocation.Proceed();
-            _cache.Add(key, invocation.ReturnValue, _duration);
-        }
+        invocation.Proceed();
+        _cache.Add(key, invocation.ReturnValue, _duration);
     }
 }
