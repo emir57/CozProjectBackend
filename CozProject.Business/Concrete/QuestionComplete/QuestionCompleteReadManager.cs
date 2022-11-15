6367e18f6cbc407157e45b27271150b6f33ff7f8
emir57 <emir.gurbuz06@hotmail.com>
2022-11-15 20:38:44 +0300

    QuestionCompleteReadManager ->Refactoring codes.

--- CozProject.Business/Concrete/QuestionComplete/QuestionCompleteReadManager.cs
+++ CozProject.Business/Concrete/QuestionComplete/QuestionCompleteReadManager.cs
@@ -4,26 +4,23 @@ using CozProject.Business.Abstract;
 using CozProject.DataAccess.Abstract;
 using System.Threading.Tasks;
 
-namespace CozProject.Business.Concrete.QuestionComplete
-{
-    public class QuestionCompleteReadManager : IQuestionCompleteReadService
-    {
-        private readonly ILanguageMessage _language;
-        private readonly IQuestionCompleteReadDal _questionCompleteReadDal;
+namespace CozProject.Business.Concrete.QuestionComplete;
 
-        public QuestionCompleteReadManager(IQuestionCompleteReadDal questionCompleteReadDal, ILanguageMessage language)
+public class QuestionCompleteReadManager : IQuestionCompleteReadService
+{
+    private readonly IQuestionCompleteReadDal _questionCompleteReadDal;
+
+    public QuestionCompleteReadManager(IQuestionCompleteReadDal questionCompleteReadDal)
+    {
+        _questionCompleteReadDal = questionCompleteReadDal;
+    }
+    public async Task<IDataResult<bool>> CheckQuestionCompleteAsync(int userId, int questionId)
+    {
+        Entities.Concrete.QuestionComplete questionComplete = await _questionCompleteReadDal.GetAsync(x => x.UserId == userId && x.QuestionId == questionId);
+        if (questionComplete is null)
         {
-            _questionCompleteReadDal = questionCompleteReadDal;
-            _language = language;
+            return new SuccessDataResult<bool>(true);
         }
-        public async Task<IDataResult<bool>> CheckQuestionCompleteAsync(int userId, int questionId)
-        {
-            var categoryComplete = await _questionCompleteReadDal.GetAsync(x => x.UserId == userId && x.QuestionId == questionId);
-            if (categoryComplete == null)
-            {
-                return new SuccessDataResult<bool>(true);
-            }
-            return new SuccessDataResult<bool>(false);
-        }
+        return new SuccessDataResult<bool>(false);
     }
 }
