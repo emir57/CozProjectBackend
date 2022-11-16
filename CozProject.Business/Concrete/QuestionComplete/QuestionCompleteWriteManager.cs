a90cfa365773c222f2e8b6f63805a0ad247f5823
emir57 <emir.gurbuz06@hotmail.com>
2022-11-16 19:24:57 +0300

    Refactoring QuestionCompleteWriteManager codes.

--- CozProject.Business/Concrete/QuestionComplete/QuestionCompleteWriteManager.cs
+++ CozProject.Business/Concrete/QuestionComplete/QuestionCompleteWriteManager.cs
@@ -4,36 +4,32 @@ using CozProject.Business.Abstract;
 using CozProject.DataAccess.Abstract;
 using System.Threading.Tasks;
 
-namespace CozProject.Business.Concrete.QuestionComplete
+namespace CozProject.Business.Concrete.QuestionComplete;
+
+public class QuestionCompleteWriteManager : IQuestionCompleteWriteService
 {
-    public class QuestionCompleteWriteManager : IQuestionCompleteWriteService
+    private readonly ILanguageMessage _language;
+    private readonly IQuestionCompleteWriteDal _questionCompleteWriteDal;
+
+    public QuestionCompleteWriteManager(IQuestionCompleteWriteDal questionCompleteWriteDal, ILanguageMessage language)
     {
-        private readonly ILanguageMessage _language;
-        private readonly IQuestionCompleteWriteDal _questionCompleteWriteDal;
-
-        public QuestionCompleteWriteManager(IQuestionCompleteWriteDal questionCompleteWriteDal, ILanguageMessage language)
-        {
-            _questionCompleteWriteDal = questionCompleteWriteDal;
-            _language = language;
-        }
-
-        public async Task<IResult> AddAsync(Entities.Concrete.QuestionComplete questionComplete)
-        {
-            var result = await _questionCompleteWriteDal.AddAsync(questionComplete);
-            if (result)
-                return new SuccessResult(_language.SuccessAdd);
-            return new ErrorResult(_language.FailureAdd);
-        }
-
-        public IResult Delete(Entities.Concrete.QuestionComplete questionComplete)
-        {
-            _questionCompleteWriteDal.Delete(questionComplete);
-            return new SuccessResult(_language.SuccessDelete);
-        }
-
-        public Task<int> SaveAsync()
-        {
-            return _questionCompleteWriteDal.SaveAsync();
-        }
+        _questionCompleteWriteDal = questionCompleteWriteDal;
+        _language = language;
     }
+
+    public async Task<IResult> AddAsync(Entities.Concrete.QuestionComplete questionComplete)
+    {
+        bool result = await _questionCompleteWriteDal.AddAsync(questionComplete);
+        if (result)
+            return new SuccessResult(_language.SuccessAdd);
+        return new ErrorResult(_language.FailureAdd);
+    }
+
+    public IResult Delete(Entities.Concrete.QuestionComplete questionComplete)
+    {
+        _questionCompleteWriteDal.Delete(questionComplete);
+        return new SuccessResult(_language.SuccessDelete);
+    }
+
+    public Task<int> SaveAsync() => _questionCompleteWriteDal.SaveAsync();
 }
