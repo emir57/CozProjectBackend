72560c2d25559b46e1750bd4ffa7d562b99c95d7
emir57 <emir.gurbuz06@hotmail.com>
2022-11-27 21:07:46 +0300

    Remove DbContext in Dependency

--- CozProject.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ CozProject.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -7,61 +7,54 @@ using CozProject.Business.Concrete;
 using CozProject.Business.Concrete.QuestionComplete;
 using CozProject.DataAccess.Abstract;
 using CozProject.DataAccess.Concrete.EntityFramework;
-using CozProject.DataAccess.Contexts;
-using Microsoft.EntityFrameworkCore;
 
-namespace CozProject.Business.DependencyResolvers.Autofac
+namespace CozProject.Business.DependencyResolvers.Autofac;
+
+public class AutofacBusinessModule : Module
 {
-    public class AutofacBusinessModule : Module
+    protected override void Load(ContainerBuilder builder)
     {
-        protected override void Load(ContainerBuilder builder)
-        {
-            #region JWT
-            builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
-            #endregion
+        #region JWT
+        builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
+        #endregion
 
-            #region Context
-            builder.RegisterType<CozProjectDbContext>().As<DbContext>();
-            #endregion
+        #region DataAccess
+        builder.RegisterType<EfRoleWriteDal>().As<IRoleWriteDal>();
+        builder.RegisterType<EfRoleReadDal>().As<IRoleReadDal>();
+        builder.RegisterType<EfUserWriteDal>().As<IUserWriteDal>();
+        builder.RegisterType<EfUserReadDal>().As<IUserReadDal>();
+        builder.RegisterType<EfQuestionWriteDal>().As<IQuestionWriteDal>();
+        builder.RegisterType<EfQuestionReadDal>().As<IQuestionReadDal>();
+        builder.RegisterType<EfAnswerReadDal>().As<IAnswerReadDal>();
+        builder.RegisterType<EfAnswerWriteDal>().As<IAnswerWriteDal>();
+        builder.RegisterType<EfCategoryReadDal>().As<ICategoryReadDal>();
+        builder.RegisterType<EfCategoryWriteDal>().As<ICategoryWriteDal>();
+        builder.RegisterType<EfCategoryCompleteReadDal>().As<ICategoryCompleteReadDal>();
+        builder.RegisterType<EfCategoryCompleteWriteDal>().As<ICategoryCompleteWriteDal>();
+        builder.RegisterType<EfQuestionCompleteReadDal>().As<IQuestionCompleteReadDal>();
+        builder.RegisterType<EfQuestionCompleteWriteDal>().As<IQuestionCompleteWriteDal>();
+        #endregion
 
-            #region DataAccess
-            builder.RegisterType<EfRoleWriteDal>().As<IRoleWriteDal>();
-            builder.RegisterType<EfRoleReadDal>().As<IRoleReadDal>();
-            builder.RegisterType<EfUserWriteDal>().As<IUserWriteDal>();
-            builder.RegisterType<EfUserReadDal>().As<IUserReadDal>();
-            builder.RegisterType<EfQuestionWriteDal>().As<IQuestionWriteDal>();
-            builder.RegisterType<EfQuestionReadDal>().As<IQuestionReadDal>();
-            builder.RegisterType<EfAnswerReadDal>().As<IAnswerReadDal>();
-            builder.RegisterType<EfAnswerWriteDal>().As<IAnswerWriteDal>();
-            builder.RegisterType<EfCategoryReadDal>().As<ICategoryReadDal>();
-            builder.RegisterType<EfCategoryWriteDal>().As<ICategoryWriteDal>();
-            builder.RegisterType<EfCategoryCompleteReadDal>().As<ICategoryCompleteReadDal>();
-            builder.RegisterType<EfCategoryCompleteWriteDal>().As<ICategoryCompleteWriteDal>();
-            builder.RegisterType<EfQuestionCompleteReadDal>().As<IQuestionCompleteReadDal>();
-            builder.RegisterType<EfQuestionCompleteWriteDal>().As<IQuestionCompleteWriteDal>();
-            #endregion
+        #region Services
+        builder.RegisterType<RoleReadManager>().As<IRoleReadService>();
+        builder.RegisterType<RoleWriteManager>().As<IRoleWriteService>();
+        builder.RegisterType<UserReadManager>().As<IUserReadService>();
+        builder.RegisterType<UserWriteManager>().As<IUserWriteService>();
+        builder.RegisterType<CategoryCompleteReadManager>().As<ICategoryCompleteReadService>();
+        builder.RegisterType<CategoryCompleteWriteManager>().As<ICategoryCompleteWriteService>();
+        builder.RegisterType<QuestionCompleteReadManager>().As<IQuestionCompleteReadService>();
+        builder.RegisterType<QuestionCompleteWriteManager>().As<IQuestionCompleteWriteService>();
+        builder.RegisterType<AnswerReadManager>().As<IAnswerReadService>();
+        builder.RegisterType<AnswerWriteManager>().As<IAnswerWriteService>();
+        #endregion
 
-            #region Services
-            builder.RegisterType<RoleReadManager>().As<IRoleReadService>();
-            builder.RegisterType<RoleWriteManager>().As<IRoleWriteService>();
-            builder.RegisterType<UserReadManager>().As<IUserReadService>();
-            builder.RegisterType<UserWriteManager>().As<IUserWriteService>();
-            builder.RegisterType<CategoryCompleteReadManager>().As<ICategoryCompleteReadService>();
-            builder.RegisterType<CategoryCompleteWriteManager>().As<ICategoryCompleteWriteService>();
-            builder.RegisterType<QuestionCompleteReadManager>().As<IQuestionCompleteReadService>();
-            builder.RegisterType<QuestionCompleteWriteManager>().As<IQuestionCompleteWriteService>();
-            builder.RegisterType<AnswerReadManager>().As<IAnswerReadService>();
-            builder.RegisterType<AnswerWriteManager>().As<IAnswerWriteService>();
-            #endregion
-
-            #region Register Assembly
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
-                .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions
-                {
-                    Selector = new AspectInterceptorSelector()
-                });
-            #endregion
-        }
+        #region Register Assembly
+        var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+            .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions
+            {
+                Selector = new AspectInterceptorSelector()
+            });
+        #endregion
     }
 }
--- CozProject.Business/DependencyResolvers/Microsoft/MicrosoftBusinessModuleExtension.cs
+++ CozProject.Business/DependencyResolvers/Microsoft/MicrosoftBusinessModuleExtension.cs
@@ -1,34 +1,30 @@
 ﻿using Core.Utilities.Security.JWT;
-using CozProject.DataAccess.Abstract;
-using CozProject.DataAccess.Concrete.EntityFramework;
-using CozProject.DataAccess.Contexts;
 using CozProject.Business.Abstract;
 using CozProject.Business.Concrete;
-using Microsoft.EntityFrameworkCore;
+using CozProject.DataAccess.Abstract;
+using CozProject.DataAccess.Concrete.EntityFramework;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CozProject.Business.DependencyResolvers.Microsoft
+namespace CozProject.Business.DependencyResolvers.Microsoft;
+
+public static class MicrosoftBusinessModuleExtension
 {
-    public static class MicrosoftBusinessModuleExtension
+    public static IServiceCollection AddMicrosoftBusinessModule(this IServiceCollection services)
     {
-        public static IServiceCollection AddMicrosoftBusinessModule(this IServiceCollection services)
-        {
-            services.AddSingleton<ITokenHelper, JwtHelper>();
-            services.AddScoped<DbContext, CozProjectDbContext>();
-            services.AddScoped<IRoleWriteDal, EfRoleWriteDal>();
-            services.AddScoped<IRoleReadDal, EfRoleReadDal>();
-            services.AddScoped<IRoleReadService, RoleReadManager>();
-            services.AddScoped<IRoleWriteService, RoleWriteManager>();
+        services.AddSingleton<ITokenHelper, JwtHelper>();
+        services.AddScoped<IRoleWriteDal, EfRoleWriteDal>();
+        services.AddScoped<IRoleReadDal, EfRoleReadDal>();
+        services.AddScoped<IRoleReadService, RoleReadManager>();
+        services.AddScoped<IRoleWriteService, RoleWriteManager>();
 
-            services.AddScoped<IQuestionReadDal, EfQuestionReadDal>();
-            services.AddScoped<IQuestionWriteDal, EfQuestionWriteDal>();
+        services.AddScoped<IQuestionReadDal, EfQuestionReadDal>();
+        services.AddScoped<IQuestionWriteDal, EfQuestionWriteDal>();
 
-            services.AddScoped<IAnswerReadDal, EfAnswerReadDal>();
-            services.AddScoped<IAnswerWriteDal, EfAnswerWriteDal>();
+        services.AddScoped<IAnswerReadDal, EfAnswerReadDal>();
+        services.AddScoped<IAnswerWriteDal, EfAnswerWriteDal>();
 
-            services.AddScoped<ICategoryReadDal, EfCategoryReadDal>();
-            services.AddScoped<ICategoryWriteDal, EfCategoryWriteDal>();
-            return services;
-        }
+        services.AddScoped<ICategoryReadDal, EfCategoryReadDal>();
+        services.AddScoped<ICategoryWriteDal, EfCategoryWriteDal>();
+        return services;
     }
 }
